6f49813e727b7f6283e8283bac28d4e547eb319f
mobi1992 <mobeenamushtaq2@gmail.com>
2022-04-12 20:17:07 +0500

    solving cors error try2

--- src/index.js
+++ src/index.js
@@ -12,15 +12,15 @@ app.use(express.json())
 app.use(productRouter)
 app.use(categoryRouter)
 // to solve CORS policiy error when frontend tries to access our server
-app.use(cors());
+app.use(cors({origin : "http://localhost:3000", credentials : true}));
 app.options("*", cors());
 
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+// app.all('*', function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// });
 
 app.listen(port, () => {
     console.log('server is up at port ' + port)
