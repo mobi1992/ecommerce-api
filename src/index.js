f1a28ad6800ce9e8b5e35186bc2c52791aad64c7
mobi1992 <mobeenamushtaq2@gmail.com>
2022-04-12 20:09:44 +0500

    solving cors error try1

--- src/index.js
+++ src/index.js
@@ -15,6 +15,13 @@ app.use(categoryRouter)
 app.use(cors());
 app.options("*", cors());
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.listen(port, () => {
     console.log('server is up at port ' + port)
 })
